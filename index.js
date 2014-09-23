45d3f2f7d23919ac7668a356944a73924a5b3621
jden <jason@denizac.org>
2014-09-23 09:42:17 -0700

    refactor filters

--- index.js
+++ index.js
@@ -9,28 +9,51 @@ function randomRevisitor (type, cb) {
 
   request.get('http://hub.revisit.link/services',{json:true}, function (err, res) {
     if (err) { return cb(err) }
-    var rando = rand(res.body.services, type)
+    var rando = rand(
+                  where(
+                    res.body.services, [
+                      isOnline,
+                      supports(type)
+                    ]
+                  )
+                )
+
     if(!rando) { return cb(new Error('No online services found' + (type ? ' that support ' + type : '')))}
     cb(null, rando.url)
   })
 }
 
-function rand (arr, type) {
-  if(type) {arr = filterByType(arr, type)}
-  arr = arr.filter(onlineOnly)
+function rand (arr) {
   return arr[Math.floor(Math.random()*arr.length)]
 }
 
-function filterByType (arr, type) {
-  if(type === 'jpg'){type='jpeg'}
-  function supportsType(svc) {
-      return svc.supports.indexOf(type) > -1
-  }
-  return arr.filter(supportsType)
+function where(arr, predicates) {
+  return arr.filter(function (el) {
+    return predicates.every(function (predicate) {
+      return predicate(el)
+    })
+  })
 }
 
-function onlineOnly (svc) {
-    return svc.online
+function supports (type) {
+  // if type is not specified, pass through
+  if (!type) {
+    return function () { return true }
+  }
+
+
+  if (type === 'jpg') {
+    type = 'jpeg'
+  }
+
+  return function supportsType(service) {
+      return type && service.supports.indexOf(type) > -1
+  }
+
+}
+
+function isOnline (service) {
+    return service.online
 }
 
 module.exports = randomRevisitor
